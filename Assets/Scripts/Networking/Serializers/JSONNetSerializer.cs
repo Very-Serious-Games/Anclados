89c742647a2f50d9dfc9c9d073180809d120b01e
Hugo Planell Moreno <69766683+hugoplanell@users.noreply.github.com>
2025-10-22 11:14:09 +0200

    JSon serializer done

--- Assets/Scripts/Networking/Serializers/JSONNetSerializer.cs
+++ Assets/Scripts/Networking/Serializers/JSONNetSerializer.cs
@@ -1,6 +1,18 @@
+using System.Text;
 using UnityEngine;
 
-public class JSONNetSerializer
+public class JSONNetSerializer : INetworkSerializer
 {
-    
+    // TODO: Think if should recieve a INetworkMessage or just a generic T and convert to INetworkMessage inside
+    public byte[] Serialize<T>(T message) where T : INetworkMessage
+    {
+        var messageJson = JsonUtility.ToJson(message);
+        return Encoding.UTF8.GetBytes(messageJson);
+    }
+
+    public T Deserialize<T>(byte[] data) where T : INetworkMessage
+    {
+        var messageJson = Encoding.UTF8.GetString(data);
+        return JsonUtility.FromJson<T>(messageJson);
+    }
 }
