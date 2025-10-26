427aab4f5b921493e15c8ca9ed3f8bb1c190b4c8
Hugo Planell <hugoplanellmoreno@gmail.com>
2025-10-26 19:23:29 +0100

    Update PlayerChatMessage.cs

--- Assets/Scripts/Messages/PlayerChatMessage.cs
+++ Assets/Scripts/Messages/PlayerChatMessage.cs
@@ -1,7 +1,12 @@
+using UnityEngine;
+
 public struct PlayerChatMessage : INetworkMessage
 {
+    [SerializeField]
     public string message;
+    [SerializeField]
     public string username;
+    [SerializeField]
     public float timestamp;
 
     public PlayerChatMessage(string message, string username, float timestamp)
